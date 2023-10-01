6d9852e6f940fcab2f7e4cf9198ca4db31688433
David-EU-PL <dawidplatek.eu.pl@outlook.com>
2023-10-01 18:16:37 +0200

    Next day learn.

--- StudentsDiary/Main.cs
+++ StudentsDiary/Main.cs
@@ -1,70 +1,83 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Serialization;
 
 namespace StudentsDiary
 {
     public partial class Main : Form
     {
-        private string _filePath = Path.Combine(Environment.CurrentDirectory, "students.txt");
+
+        private FileHelper<List<Student>> _fileHelper = new FileHelper<List<Student>>(Program.FilePath);
 
         public Main()
         {
             InitializeComponent();
+            RefreshDiary();
+            SetColumnsHeader();
 
         }
 
-        public void SerializeToFile(List<Student> students)
+        private void RefreshDiary()
         {
-            var serializer = new XmlSerializer(typeof(List<Student>));
-            using (var streamWriter = new StreamWriter(_filePath))
-            {
-                serializer.Serialize(streamWriter, students);
-                streamWriter.Close();
-            }
+            var students = _fileHelper.DeserializeFromFile();
+            dgvDiary.DataSource = students;
         }
-
-        public List<Student> DeserializeFromFile()
+        private void SetColumnsHeader()
         {
-            if (!File.Exists(_filePath))
-                return new List<Student>();
-
-            var serializer = new XmlSerializer(typeof(List<Student>));
-            using (var streamReader = new StreamReader(_filePath))
-            {
-                var students = (List<Student>)serializer.Deserialize(streamReader);
-                return students;
-            }
+            dgvDiary.Columns[0].HeaderText = "Numer";
+            dgvDiary.Columns[1].HeaderText = "Imię";
+            dgvDiary.Columns[2].HeaderText = "Nazwisko";
+            dgvDiary.Columns[3].HeaderText = "Uwagi";
+            dgvDiary.Columns[4].HeaderText = "Matematyka";
+            dgvDiary.Columns[5].HeaderText = "Technologia";
+            dgvDiary.Columns[6].HeaderText = "Fizyka";
+            dgvDiary.Columns[7].HeaderText = "Język polski";
+            dgvDiary.Columns[8].HeaderText = "Język obcy";
         }
-
-
+     
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
+            var addEditStudent = new AddEditStudent();
+            addEditStudent.ShowDialog();
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-
+            if (dgvDiary.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Zaznacz dane ucznia, którego dane chcesz edytować", "Edycja");
+                return;
+            }
+            var addEditStudent = new AddEditStudent(Convert.ToInt32(dgvDiary.SelectedRows[0].Cells[0].Value));
+            addEditStudent.ShowDialog();
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
+            if (dgvDiary.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Zaznacz dane ucznia, którego dane chcesz usunąć", "Usuń");
+                return;
+            }
 
+            var selectedStudent = dgvDiary.SelectedRows[0];
+            var confirmDelete = MessageBox.Show($"Czy chcesz usunąć ucznia {(selectedStudent.Cells[1].Value.ToString() + " " + selectedStudent.Cells[2].Value.ToString()).Trim()}?", "Usuwanie ucznia", MessageBoxButtons.OKCancel);
+
+            if (confirmDelete == DialogResult.OK)
+            {
+                DeleteStudent(Convert.ToInt32(selectedStudent.Cells[0].Value));
+                RefreshDiary();
+            }
         }
-
+        private void DeleteStudent(int id)
+        {
+            var students = _fileHelper.DeserializeFromFile();
+            students.RemoveAll(x => x.Id == id);
+            _fileHelper.SerializeToFile(students);
+        }
         private void btnRefresh_Click(object sender, EventArgs e)
         {
-
+            RefreshDiary();
         }
     }
 }
